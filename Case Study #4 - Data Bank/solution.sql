9ec1b863b43d2e1eb920cfb4908dc779bbce21d1
Indrajeet Mishra <indrajeetmishra@thecoddiwompler.local>
2023-12-04 11:32:43 +0530

    Added the solution of part B in case study 4

--- Case Study #4 - Data Bank/solution.sql
+++ Case Study #4 - Data Bank/solution.sql
@@ -138,4 +138,241 @@ FROM
     INNER JOIN percentile_95th c ON a.region_id = c.region_id
     INNER JOIN data_bank.regions d ON d.region_id = a.region_id
 ORDER BY
-    1
+    1;
+
+
+-- B. Customer Transactions
+
+-- 6. What is the unique count and total amount for each transaction type?
+
+SELECT
+	txn_type,
+	COUNT(*) unique_count,
+	SUM(txn_amount) total_amount
+FROM
+	data_bank.customer_transactions
+GROUP BY
+	txn_type;
+
+
+-- 7. What is the average total historical deposit counts and amounts for all customers?
+
+WITH cte AS (
+	SELECT
+		customer_id,
+		COUNT(txn_amount) total_historical_deposit_count,
+		SUM(txn_amount) total_historical_deposit_sum
+	FROM
+		data_bank.customer_transactions
+	WHERE
+		txn_type = 'deposit'
+	GROUP BY
+		customer_id
+)
+SELECT
+	ROUND(AVG(total_historical_deposit_count), 2) avg_total_historical_deposit_count,
+	ROUND(AVG(total_historical_deposit_sum), 2) avg_total_historical_deposit_sum
+FROM
+	cte;
+
+
+-- 8. For each month - how many Data Bank customers make more than 1 deposit and either 1 purchase or 1 withdrawal in a single month?
+
+WITH cte AS (
+	SELECT
+		TO_CHAR(txn_date, 'MonthYYYY') month_name,
+		customer_id,
+		COUNT(txn_type) FILTER(
+			WHERE
+				txn_type = 'deposit'
+		) deposit_count,
+		COUNT(txn_type) FILTER(
+			WHERE
+				txn_type = 'withdrawal'
+		) withdrawal_count,
+		COUNT(txn_type) FILTER(
+			WHERE
+				txn_type = 'purchase'
+		) purchase_count
+	FROM
+		data_bank.customer_transactions
+	GROUP BY
+		TO_CHAR(txn_date, 'MonthYYYY'),
+		customer_id
+)
+SELECT
+	month_name,
+	COUNT(customer_id)
+FROM
+	cte
+WHERE
+	deposit_count > 1
+	AND (
+		withdrawal_count = 1
+		OR purchase_count = 1
+	)
+GROUP BY
+	month_name;
+
+
+-- 9. What is the closing balance for each customer at the end of the month?
+
+WITH all_months AS (
+	SELECT
+		DISTINCT TO_CHAR(txn_date, 'MonthYYYY') month_name,
+		TO_CHAR(txn_date, 'MM') month_num,
+		TO_CHAR(txn_date, 'YY') year_num
+	FROM
+		data_bank.customer_transactions
+),
+all_customers AS (
+	SELECT
+		DISTINCT customer_id
+	FROM
+		data_bank.customer_transactions
+),
+all_detailed_customers AS (
+	SELECT
+		a.customer_id,
+		b.month_name,
+		b.month_num,
+		b.year_num
+	FROM
+		all_customers a
+		CROSS JOIN all_months b
+),
+all_transactions AS (
+	SELECT
+		customer_id,
+		TO_CHAR(txn_date, 'MonthYYYY') month_name,
+		TO_CHAR(txn_date, 'MM') month_num,
+		TO_CHAR(txn_date, 'YY') year_num,
+		SUM(
+			CASE
+				WHEN txn_type = 'deposit' THEN txn_amount
+				ELSE txn_amount *(-1)
+			END
+		) total_transaction
+	FROM
+		data_bank.customer_transactions
+	GROUP BY
+		customer_id,
+		TO_CHAR(txn_date, 'MonthYYYY'),
+		TO_CHAR(txn_date, 'MM'),
+		TO_CHAR(txn_date, 'YY')
+),
+all_detailed_transactions AS (
+	SELECT
+		a.customer_id,
+		a.month_name,
+		a.month_num,
+		a.year_num,
+		COALESCE(b.total_transaction, 0) total_transaction
+	FROM
+		all_detailed_customers a
+		LEFT OUTER JOIN all_transactions b ON a.month_name = b.month_name
+		AND a.customer_id = b.customer_id
+)
+SELECT
+	customer_id,
+	month_name,
+	total_transaction,
+	SUM(total_transaction) OVER(
+		PARTITION BY customer_id
+		ORDER BY
+			year_num,
+			month_num
+	) month_end_balance
+FROM
+	all_detailed_transactions
+ORDER BY
+	customer_id
+
+
+-- 10. What is the percentage of customers who increase their closing balance by more than 5%?
+
+-- Assumption- It has been assumed that the question is asking for the percentage of customers who increased their closing balance by more than 5% in every single month.
+
+WITH all_transactions AS (
+  SELECT
+    customer_id,
+    TO_CHAR(txn_date, 'MonthYYYY') month_name,
+    TO_CHAR(txn_date, 'MM') month_num,
+    TO_CHAR(txn_date, 'YY') year_num,
+    SUM(
+      CASE
+        WHEN txn_type = 'deposit' THEN txn_amount
+        ELSE txn_amount *(-1)
+      END
+    ) total_transaction
+  FROM
+    data_bank.customer_transactions
+  GROUP BY
+    customer_id,
+    TO_CHAR(txn_date, 'MonthYYYY'),
+    TO_CHAR(txn_date, 'MM'),
+    TO_CHAR(txn_date, 'YY')
+),
+month_end_balance AS (
+  SELECT
+    customer_id,
+    month_name,
+    month_num,
+    year_num,
+    SUM(total_transaction) OVER(
+      PARTITION BY customer_id
+      ORDER BY
+        year_num,
+        month_num
+    ) month_end_balance
+  FROM
+    all_transactions
+),
+month_end_bal_perc_change AS (
+  SELECT
+    customer_id,
+    month_name,
+    month_end_balance,
+    CASE
+      WHEN LAG(month_end_balance) OVER(
+        PARTITION BY customer_id
+        ORDER BY
+          year_num,
+          month_num
+      ) != 0 THEN (
+        month_end_balance - LAG(month_end_balance) OVER(
+          PARTITION BY customer_id
+          ORDER BY
+            year_num,
+            month_num
+        )
+      ) * 100.0 / LAG(month_end_balance) OVER(
+        PARTITION BY customer_id
+        ORDER BY
+          year_num,
+          month_num
+      )
+      ELSE NULL
+    END AS percentage_change
+  FROM
+    month_end_balance
+),
+all_customers AS (
+  SELECT
+    customer_id,
+    MIN(percentage_change) min_percentage_change
+  FROM
+    month_end_bal_perc_change
+  GROUP BY
+    customer_id
+)
+SELECT
+  ROUND(
+    COUNT(customer_id) FILTER(
+      WHERE
+        min_percentage_change > 5
+    ) * 100.0 / COUNT(customer_id),
+    2
+  ) customer_percentage
+FROM
+  all_customers
